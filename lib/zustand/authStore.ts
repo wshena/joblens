5a77bb6dbae7cea64fe6baa1b5a36578b42e952e
wshena <wshenaarrafi@gmail.com>
2026-06-08 13:26:41 +0700

    feat:create zustand store (auth & utility)

---
+++ lib/zustand/authStore.ts
@@ -0,0 +1,23 @@
+import type { Session, User } from "@supabase/supabase-js";
+import { create } from "zustand";
+
+interface AuthState {
+  user: User | null;
+  session: Session | null;
+
+  // Actions
+  getUser: (user: User | null) => void;
+  getSession: (session: Session | null) => void;
+  logoutUser: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  // Initial state
+  user: null,
+  session: null,
+
+  // Actions
+  getUser: (user) => set({ user }),
+  getSession: (session) => set({ session }),
+  logoutUser: () => set({ user: null, session: null }),
+}));
