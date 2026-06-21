1ec4a0247c22b68f79720883664ac836ce353757
wshena <wshenaarrafi@gmail.com>
2026-06-21 13:38:58 +0700

    feat: create ModalContainer component and add it to MainContainer

--- components/container/MainContainer.tsx
+++ components/container/MainContainer.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import React from "react";
+import ModalContainer from "./ModalContainer";
+import { useUtilityStore } from "@/lib/zustand/utilityStore";
 
 interface MainContainerProps {
   children: React.ReactNode;
 }
 
 const MainContainer = ({ children }: MainContainerProps) => {
+  const { isModalOpen, modalContent, modalOptions, closeModal } =
+    useUtilityStore();
+
   return (
     <div className="relative min-h-screen w-full">
+      {/* modal container */}
+      <ModalContainer
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        showOverlay={modalOptions.showOverlay}
+        closeOnOverlayClick={modalOptions.closeOnOverlayClick}
+        overlayClassName={modalOptions.overlayClassName}
+        contentClassName={modalOptions.contentClassName}
+      >
+        {modalContent}
+      </ModalContainer>
+
       {/* Page Content */}
       {children}
     </div>
